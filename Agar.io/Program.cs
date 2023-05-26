4b8d8fca523df2e0526f0262e4fe44bc25c14b6f
BlackListed <yura.riddig@gmail.com>
2023-05-26 16:56:57 +0300

    Init Commit

---
+++ Agar.io/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Agar.io
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
