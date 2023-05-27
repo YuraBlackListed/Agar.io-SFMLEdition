cbabd8bad85792741799d7da5cbefe62ba9e6883
BlackListed <yura.riddig@gmail.com>
2023-05-27 09:47:55 +0300

    Added Extention Methods

---
+++ Agario/Engine/ExtensionMethods/GameObjectExtentionMethods.cs
@@ -0,0 +1,12 @@
+﻿using SFMLGame.Engine;
+
+namespace Agario.Engine.ExtensionMethods.GameObjectExtentionMethods
+{
+    public static class GameObjectExtentionMethods
+    {
+        public static bool CollidesWith(this GameObject object1, GameObject object2)
+        {
+            return object1.Mesh.GetGlobalBounds().Intersects(object2.Mesh.GetGlobalBounds());
+        }
+    }
+}
