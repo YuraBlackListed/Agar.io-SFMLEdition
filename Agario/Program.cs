8e870eb2bf0cb71d31ded89222c900833d3293ee
BlackListed <yura.riddig@gmail.com>
2023-05-30 08:12:49 +0300

    Removed unneeded usings

--- Agario/Program.cs
+++ Agario/Program.cs
@@ -1,14 +1,10 @@
-﻿using SFML.System;
-using SFML.Graphics;
-using SFML.Window;
-
-namespace Agario
+﻿namespace Agario
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Game.Game game = new Game.Game(1000);
+            Game.Game game = new Game.Game(100);
             game.Start();
         }
     }
